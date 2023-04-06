b29e5a02129b7519b4ab2f22652c8e7bfcc5c7c7
demerson <demersontorres520@gmail.com>
2023-04-05 21:39:21 -0300

    Aula #94 criando a tela de cadastro

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -12,9 +12,10 @@ import {
   SubmitText,
   Link,
   LinkText,
-} from "./styles";
+} from "../SignIn/styles";
 
-export default function SignIn() {
+export default function SignUp() {
+  const [nome, setNome] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const navigation = useNavigation()
@@ -25,7 +26,15 @@ export default function SignIn() {
         behavior={Platform.OS === "ios" ? "padding" : ""}
         enabled
       >
-        <Logo source={require("../../assets/Logo.png")} />
+        <AreaInput>
+          <Input
+            placeholder="Nome"
+            autoCorrect={false}
+            autoCapitalize="none"
+            value={nome}
+            onChangeText={(text) => setNome(text)}
+          />
+        </AreaInput>
         <AreaInput>
           <Input
             placeholder="Email"
@@ -47,8 +56,8 @@ export default function SignIn() {
         <SubmitButton>
           <SubmitText>Acessar</SubmitText>
         </SubmitButton>
-        <Link onPress={() => navigation.navigate("SignUp")}>
-          <LinkText>Criar uma conta!</LinkText>
+        <Link onPress={() => navigation.navigate("SignIn")}>
+          <LinkText>Entrar em uma conta!</LinkText>
         </Link>
       </Container>
     </Background>
