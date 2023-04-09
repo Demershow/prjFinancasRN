c3bec717be5b1cc14b893a5693d1f86f82c3af28
demerson <demersontorres520@gmail.com>
2023-04-09 18:33:35 -0300

    aula #103 registrando um gasto

--- src/pages/New/index.js
+++ src/pages/New/index.js
@@ -1,5 +1,8 @@
-import React, {useState} from 'react';
-import { TouchableWithoutFeedback, Keyboard } from 'react-native';
+import React, {useState, useContext} from 'react';
+import { TouchableWithoutFeedback, Keyboard, Alert } from 'react-native';
+import firebase from '../../services/firebaseConnection';
+import { format } from 'date-fns';
+import { AuthContext } from '../../contexts/auth';
 
 import Header from '../../components/Header';
 import {Background, Input, SubmitButton, SubmitText} from './style';
@@ -11,6 +14,59 @@ export default function New() {
 
   const [valor, setValor] = useState('');
   const [tipo, setTipo] = useState(null);	
+
+  const { user: usuario } = useContext(AuthContext);
+
+  function handleSubmit() {
+    Keyboard.dismiss();
+    if(isNaN(parseFloat(valor)) || tipo === null) {
+      alert('Preencha todos os campos');
+      return;
+    }
+
+    Alert.alert(
+      'Confirmando dados',
+      `Tipo ${tipo} - Valor: ${parseFloat(valor)}`,
+      [
+        {
+          text: 'Cancelar',
+          style: 'cancel'
+        },
+        {
+          text: 'Continuar',
+          onPress: () => handleAdd()
+        }
+      ]
+    )
+
+      
+  }
+
+  async function handleAdd() {
+    let uid = usuario.uid
+
+    let key = await firebase.database().ref('historico').child(uid).push().key;
+
+    await firebase.database().ref('historico').child(uid).child(key).set({
+      tipo: tipo,
+      valor: parseFloat(valor),
+      data: format(new Date(), 'dd/MM/yyyy')
+
+    })
+
+    let user = firebase.database().ref('users').child(uid);
+    await user.once('value').then((snapshot) => {
+      let saldo = parseFloat(snapshot.val().saldo);
+
+      tipo === 'despesa' ? saldo -= parseFloat(valor) : saldo += parseFloat(valor);
+
+      user.child('saldo').set(saldo);
+    })
+
+    Keyboard.dismiss();
+    setValor('');
+    setTipo(null);
+  }
 
  return (
   <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
@@ -29,7 +85,7 @@ export default function New() {
 
         <Picker onChange={setTipo} tipo={tipo} />
 
-        <SubmitButton>
+        <SubmitButton onPress={()=> handleSubmit()}>
           <SubmitText>
             Registrar
           </SubmitText>
