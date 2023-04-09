79938227f33d507ceb89432428118de526e6497d
demerson <demersontorres520@gmail.com>
2023-04-09 18:41:08 -0300

    Aula #104 Mostrando registros

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,34 +1,52 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect, useContext} from 'react';
 
 import { AuthContext } from '../../contexts/auth';
+
+import firebase from '../../services/firebase';
 
 import Header from '../../components/Header';
 import HistoricoList from '../../components/historicoList';
 
 import { Container, Background, Nome, Saldo, Title, List } from './styles.js';
+import { format } from 'date-fns';
 
 export default function Home() {
-  const [saldo, setSaldo] = useState(123.45);
-  const [historico, setHistorico] = useState([
-    {key: '1', tipo: 'despesa', valor: 10.00, data: new Date()},
-    {key: '2', tipo: 'receita', valor: 15.00, data: new Date()},
-    {key: '3', tipo: 'receita', valor: 1129.00, data: new Date()},
-    {key: '4', tipo: 'despesa', valor: 159.00, data: new Date()},
-    {key: '5', tipo: 'receita', valor: 199.00, data: new Date()},
-    {key: '6', tipo: 'despesa', valor: 11.00, data: new Date()},
-    {key: '7', tipo: 'despesa', valor: 10.00, data: new Date()},
-    {key: '8', tipo: 'receita', valor: 160.00, data: new Date()},
-    {key: '0', tipo: 'despesa', valor: 30.00, data: new Date()},
-  ]);
+  const [saldo, setSaldo] = useState(0);
+  const [historico, setHistorico] = useState([]);
 
   const { user } = useContext(AuthContext);
+  const uid = user && user.uid
+
+  useEffect(() => {
+    async function loadList() {
+      await firebase.database().ref('users').child(uid).on('value', (snapshot) => {
+        setSaldo(snapshot.val().saldo);
+      })
+
+      await firebase.database().ref('historico').child(uid).orderByChild('date').equalTo(format(new Date, 'dd/MM/yy')).limitToLast(10).on('value', (snapshot) => {
+        setHistorico([]);
+        snapshot.forEach((childItem) => {
+          let list = {
+            key: childItem.key,
+            tipo: childItem.val().tipo,
+            valor: childItem.val().valor,
+            date: childItem.val().date,
+          };
+          setHistorico(oldArray => [...oldArray, list].reverse());
+        })
+      })
+    }
+
+    loadList();
+  }, [])
+
   
  return (
    <Background>
       <Header />
       <Container>
         <Nome>Bem vindo, {user && user.name}</Nome>
-        <Saldo>123,45</Saldo>
+        <Saldo>R$ {saldo.toFixed(2)}</Saldo>
       </Container>
 
       <Title>Ultimas movimentaçoes</Title>
